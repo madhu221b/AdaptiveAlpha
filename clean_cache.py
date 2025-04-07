90167144c9538e09c4271c2bc4089c88e52bf398
Madhura Pawar <madhurapawar10@gmail.com>
2025-04-07 18:44:17 +0200

    Update clean_cache.py

--- clean_cache.py
+++ clean_cache.py
@@ -1,7 +1,7 @@
 import os
 import re
 
-dirname = "/home/mpawar/AdaptiveAlpha/fastadaptivealphatestfixed_alpha_0.5_beta_2.0_fm_0.3"
+dirname = "./AdaptiveAlpha/fastadaptivealphatestfixed_alpha_0.5_beta_2.0_fm_0.3"
 pattern = re.compile(r"^((?!29).)*$")
 
 for filename in os.listdir(dirname):
